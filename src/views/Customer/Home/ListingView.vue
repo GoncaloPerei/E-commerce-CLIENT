82335606831fb30134464017f927f3672c87a84e
Gonçalo Pereira <83283563+GoncaloPerei@users.noreply.github.com>
2024-07-23 16:27:17 +0100

    feat: add pagination to ListingView (#3)
    * feat: add env variables
    
    Added environment variables to project. Assigned them correctly in every pinia store to make requests to micro services correctly. Added .env.example and modified git ignore
    
    * feat: add pagination to ListingView

--- src/views/Customer/Home/ListingView.vue
+++ src/views/Customer/Home/ListingView.vue
@@ -2,15 +2,23 @@
 import { onMounted, onUnmounted, onUpdated } from "vue";
 import { useRoute } from "vue-router";
 import { useProductsStore } from "@/store/ProductsService";
+import { useCrudOptionsStore } from "@/store/useCrudOptions";
 import ProductCard from "@/components/ProductCard.vue";
+import Paginator from "primevue/paginator";
 
 const route = useRoute();
 
 //Initialize Stores
 const productsStore = useProductsStore();
+const crudOptionsStore = useCrudOptionsStore();
+
+const handleChange = async () => {
+  await productsStore.getCustomerContent();
+};
 
 //Lifecycle Hooks
 onMounted(async () => {
+  productsStore.params.paginate = 10;
   productsStore.params.filter.product_status_id = 1;
   productsStore.params.filter.product_category_id = route.params.id;
   try {
@@ -43,18 +51,33 @@ onUnmounted(async () => {
 
 <template>
   <div
-    class="w-full h-screen px-5 py-6 flex flex-wrap justify-center overflow-y-scroll gap-5"
+    class="relative w-full h-auto px-5 py-6 flex flex-col justify-between overflow-y-scroll gap-5"
   >
-    <ProductCard
-      v-for="product in productsStore?.data?.data"
-      class="hover:scale-105 transition-all duration-300"
-      :title="product?.title"
-      :price="product?.price"
-      :image="product?.image"
-      :stock="product?.stock"
-      :link="'/category/' + route.params.id + '/product/' + product?.id"
-    />
-    
+    <div class="relative w-full h-full flex justify-center overflow-y-auto">
+      <div class="w-full flex flex-wrap justify-center gap-5">
+        <ProductCard
+          v-for="product in productsStore?.data?.data"
+          class="hover:scale-105 transition-all duration-300"
+          :title="product?.title"
+          :price="product?.price"
+          :image="product?.image"
+          :stock="product?.stock"
+          :link="'/category/' + route.params.id + '/product/' + product?.id"
+        />
+      </div>
+    </div>
+    <div
+      class="relative w-auto bg-[#BEBEBE]/20 flex justify-center items-center p-8 mx-5 rounded-lg"
+    >
+      <Paginator
+        v-model:first="crudOptionsStore.page"
+        @update:first="handleChange()"
+        :rows="1"
+        :totalRecords="productsStore?.data?.meta?.last_page"
+        template="PrevPageLink CurrentPageReport NextPageLink"
+        class="absolute m-auto left-0 right-0 font-bold"
+      />
+    </div>
   </div>
   <RouterView />
 </template>
